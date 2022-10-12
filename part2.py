c64bf80512664b3309541e67eabc609de2d09450
reedmccullough <reed.m.mccullough@gmail.com>
2022-10-11 23:07:18 -0700

    refactoring and part 3 progress

--- part2.py
+++ part2.py
@@ -5,37 +5,8 @@ from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 import random
 
-def main():
-    # prime number
-    p = 0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371
-    # primitive root estimate
-    g = 0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5
 
-    iv = get_random_bytes(16)
-
-    # messages being sent
-    mA = pad(b'Hi Bob!', 16, style='pkcs7')
-    mB = pad(b'Hi Alice!', 16, style='pkcs7')
-    bMalA = b'Bob I need money! Can you send me some?'
-    bMalB = b"Alice, I'm on vacation. Good luck haha"
-    mMalA = pad(bMalA, 16, style='pkcs7')
-    mMalB = pad(bMalB, 16, style='pkcs7')
-
-    # Alice
-    a = random.randint(0, p - 1)
-    a_final = pow(g, a, p)
-    # Bob
-    b = random.randint(0, p - 1)
-    b_final = pow(g, b, p)
-    # Mallory
-    m = random.randint(0, p - 1)
-    m_final = pow(g, m, p)
-
-    # ALICE ATTEMPTS TO SEND BOB a, BOB ATTEMPTS TO SEND ALICE b
-    sA = pow(b_final, a, p)
-    sB = pow(a_final, b, p)
-    sM = pow(m_final, a, p)
-
+def transformations(iv, sA, sB, mA, mB):
     # truncating SHA digest to 16 bytes for CBC encryption
     kA = SHA256.new()
     kA.update(sA.to_bytes(256, 'big'))
@@ -62,13 +33,23 @@ def main():
     print("Alice's Encrypted Message: " + str(c0))
     print("\nBob's Original Message: " + str(b'Hi Alice!'))
     print("Bob's Encrypted Message: " + str(c1))
-    
-    print("\n--- Man in the Middle Attack ---\n")
-    print("Mallory intercepts and decrypts messages from both parties")
-    
+
+    return c0, c1, decA, decB
+
+def manInMiddle(iv, sA, sB, p, mMalA, mMalB, answers):
+    bMalA = b'Bob I need money! Can you send me some?'
+    bMalB = b"Alice, I'm on vacation. Good luck haha"
+
+    kA = SHA256.new()
+    kA.update(sA.to_bytes(256, 'big'))
+    kaVal = kA.hexdigest()[:16]
+    kB = SHA256.new()
+    kB.update(sB.to_bytes(256, 'big'))
+    kbVal = kB.hexdigest()[:16]
+
     # Mallory intercepts, decrypts, and modifies their messages
     kM = SHA256.new()
-    kM.update(sM.to_bytes(256, 'big'))
+    kM.update(p.to_bytes(256, 'big'))
     kmVal = kA.hexdigest()[:16]
     boxMA = AES.new(bytes(kmVal, 'utf-8'), AES.MODE_CBC, iv)
     boxMB = AES.new(bytes(kmVal, 'utf-8'), AES.MODE_CBC, iv)
@@ -78,10 +59,10 @@ def main():
     badc0 = boxMA.encrypt(mMalA)
     badc1 = boxMB.encrypt(mMalB)
 
-    print("\nOriginal Message Mallory gets from Alice: " + str(c0))
-    print("Message Mallory decrypts from Alice: " + str(unpad(decMA.decrypt(c0), 16)))
-    print("\nOriginal Message Mallory gets from Bob: " + str(c1))
-    print("Message Mallory decrypts from Bob: " + str(unpad(decMB.decrypt(c1), 16)))
+    print("\nOriginal Message Mallory gets from Alice: " + str(answers[0]))
+    print("Message Mallory decrypts from Alice: " + str(unpad(decMA.decrypt(answers[0]), 16)))
+    print("\nOriginal Message Mallory gets from Bob: " + str(answers[1]))
+    print("Message Mallory decrypts from Bob: " + str(unpad(decMB.decrypt(answers[1]), 16)))
 
     print('\n --- Mallory encrypts and sends out different messages ---')
     print("\nNew Message being sent from Alice to Bob: \n" + str(bMalA))
@@ -97,15 +78,13 @@ def main():
     # message is different than original message, but only Mallory knows that
     print("\n--- New Outcome After Mallory's Deceit ---\n")
     print("Alice's New Message (Using only Bob's Info to Decrypt):")
-    print(str(unpad(decB.decrypt(badc0), 16)))
+    print(str(unpad(answers[2].decrypt(badc0), 16)))
 
     print("\nBob's New Message (Using only Alice's Info to Decrypt):")
-    print(str(unpad(decA.decrypt(badc1), 16)))
+    print(str(unpad(answers[3].decrypt(badc1), 16)))
     print()
 
-    # repeating the attacks, but this time by changing g to 1, p, and p-1
-    print(" --- Repeating Attack, but with this time with changing g ---")
-
+def specialTampering(iv, p, mA, mB):
     g = 1 # g = p, and g = p-1
     
     # Alice
@@ -114,18 +93,23 @@ def main():
     # Bob
     b = random.randint(0, p - 1)
     b_final = pow(g, b, p)
-    # Mallory
-    m = random.randint(0, p - 1)
-    m_final = pow(g, m, p)
+
+    sA = pow(b_final, a, p)
+    sB = pow(a_final, b, p)
+
+    kA = SHA256.new()
+    kA.update(sA.to_bytes(256, 'big'))
+    kaVal = kA.hexdigest()[:16]
+    kB = SHA256.new()
+    kB.update(sB.to_bytes(256, 'big'))
+    kbVal = kB.hexdigest()[:16]
 
     print("\nA and B when g is changed to " + str(g))
     print("A: " + str(a_final))
     print("B: " + str(b_final))
-    
-    sA = pow(b_final, a, p)
-    sB = pow(a_final, b, p)
-    sM = pow(m_final, a, p)
 
+    # Mallory can then make the encrypt and decrypt objects herself
+    # because she knows the value of g is 1, and so are the secrets
     boxA = AES.new(bytes(kaVal, 'utf-8'), AES.MODE_CBC, iv)
     decA = AES.new(bytes(kaVal, 'utf-8'), AES.MODE_CBC, iv)
 
@@ -135,27 +119,58 @@ def main():
     c0 = boxA.encrypt(mA)
     c1 = boxB.encrypt(mB)
 
-    kM = SHA256.new()
-    kM.update(sM.to_bytes(256, 'big'))
-    kmVal = kA.hexdigest()[:16]
-    boxMA = AES.new(bytes(kmVal, 'utf-8'), AES.MODE_CBC, iv)
-    boxMB = AES.new(bytes(kmVal, 'utf-8'), AES.MODE_CBC, iv)
-    decMA = AES.new(bytes(kaVal, 'utf-8'), AES.MODE_CBC, iv)
-    decMB = AES.new(bytes(kbVal, 'utf-8'), AES.MODE_CBC, iv)
-    
-    badc0 = boxMA.encrypt(mMalA)
-    badc1 = boxMB.encrypt(mMalB)
+    # message is sent, and decrypted by both parties for viewing
+    print("\nAlice's Original Message: " + str(b'Hi Bob!'))
+    print("Alice's Encrypted Message: " + str(c0))
+    print("Alice's Message after Decryption")
+    print("(Using only Bob's Info to Decrypt): " + str(unpad(decB.decrypt(c0), 16)))
 
-    print("\nMessages w/ g = " + str(g) +"\n")
-
-    print("Messages after Mallory modifies them and encryptes with g = 1")
-    print(str(badc0))
-    print(str(badc1))
-
-    print("\nNew Messages after Alice and Bob decrypt them:")
-    print(str(unpad(decB.decrypt(badc0), 16)))
-    print(str(unpad(decA.decrypt(badc1), 16)))
+    print("\nBob's Original Message: " + str(b'Hi Alice!'))
+    print("Bob's Encrypted Message: " + str(c1))
+    print("Bob's Message after Decryption")
+    print("(Using only Alice's Info to Decrypt): " + str(unpad(decA.decrypt(c1), 16)))
     print()
+
+    
+
+def main():
+    # prime number
+    p = 0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371
+    # primitive root estimate
+    g = 0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5
+
+    iv = get_random_bytes(16)
+
+    # messages being sent
+    mA = pad(b'Hi Bob!', 16, style='pkcs7')
+    mB = pad(b'Hi Alice!', 16, style='pkcs7')
+    bMalA = b'Bob I need money! Can you send me some?'
+    bMalB = b"Alice, I'm on vacation. Good luck haha"
+    mMalA = pad(bMalA, 16, style='pkcs7')
+    mMalB = pad(bMalB, 16, style='pkcs7')
+
+    # Alice
+    a = random.randint(0, p - 1)
+    a_final = pow(g, a, p)
+    # Bob
+    b = random.randint(0, p - 1)
+    b_final = pow(g, b, p)
+
+    # ALICE ATTEMPTS TO SEND BOB a, BOB ATTEMPTS TO SEND ALICE b
+    sA = pow(b_final, a, p)
+    sB = pow(a_final, b, p)
+
+    answers = transformations(iv, sA, sB, mA, mB)
+    
+    print("\n--- Man in the Middle Attack ---\n")
+    print("Mallory intercepts and decrypts messages from both parties")
+    
+    manInMiddle(iv, sA, sB, p, mMalA, mMalB, answers)
+
+    # repeating the attacks, but this time by changing g to 1, p, and p-1
+    print(" --- Repeating Attack, but with this time with changing g ---")
+
+    specialTampering(iv, p, mA, mB)
 
 
 
--- part3.py
+++ part3.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.pgen import generate_grammar
 from Crypto.Random import *
 from Crypto.Util import *
 from Crypto.Util.Padding import *
@@ -7,14 +8,17 @@ from Crypto.Cipher import AES
 import random
 
 def main():
-    # e = 65537
-    e = 7
-    p = 17
-    q = 11
+
+    message0 = b'Hi Bob!'
+    message1 = b'Hi Alice!'
+    iv = get_random_bytes(16)
+
+    e = 65537
+    p = 125964645423958069741744529872772275038609514555435541608216390604613823459381408660806745193581739918797012549128245959891659812698434245387059165798100501363151789084691139085011954092422539136437549572179511222633223746890903774122843224374028408054546752077447681061255351436619998981366579355763403617113
+    q = 152095254918764933056617056633930033605024647767903251578815696981217507850462368353063951686915545480449471554404723941192966893631764783804562619505069589457786904313605972166124992689628638482920436448339710705231485899897658846004179205102097454882808616247021503534956636236244846643236042129095806861711
     n = p * q
     et = (p-1) * (q-1)
     d = pow(e, -1, et)
-    print(d)
     # Alice sends e and n to Bob
     # Public Key (e, n)
     # Private Key (t, n)
@@ -26,13 +30,39 @@ def main():
 
     # Alice decrypts secret, both generate keys for AES
     secret = pow(c, d, n)
-    print(x, secret)
+    print("\nIs Alice's secret identical to Bob's?")
+    print("Comparison: " + str(x == secret))
 
+    # SHA256 with their secret, leads to same hash
     kA = SHA256.new()
-    kA.update(bytes(secret))
+    kA.update(secret.to_bytes(256, 'big'))
     kB = SHA256.new()
-    kB.update(bytes(x))
-    print(str(kA.hexdigest()) == str(kB.hexdigest()))
+    kB.update(x.to_bytes(256, 'big'))
+
+    # encrypt with the SHA hash, CBC, with the same iv
+    cbcAlice = AES.new(kA.digest()[:16], AES.MODE_CBC, iv)
+    cbcBob = AES.new(kB.digest()[:16], AES.MODE_CBC, iv)
+
+    # encrypt the messages and send them out
+    c0 = cbcAlice.encrypt(pad(message0, 16))
+    c1 = cbcBob.encrypt(pad(message1, 16))
+
+    # decrypt eachother's messages to find what they said!
+    decA = unpad(AES.new(kA.digest()[:16], AES.MODE_CBC, iv).decrypt(c1), 16)
+    decB = unpad(AES.new(kB.digest()[:16], AES.MODE_CBC, iv).decrypt(c0), 16)
+
+    print("\nMessages after decryption:")
+    print(decA, decB)
+
+    # Mallory knows Alice's public key, and can use it
+    # to encrypt her own messages
+    malS = pow((pow(2, e)*c), d, n) // 2
+    print(malS)
+    print()
+    print(secret)
+
+
+
 
 
 
